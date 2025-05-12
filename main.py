ad580216c2e8a90f9e2a539bac532a0e30ce488b
Ted <xup6y3ru04@gmail.com>
2025-05-12 22:45:45 +0800

    feat: add Dockerfile for containerized environment setup and simplify main function

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import time
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 
-def run_spider():
+def main():
     process = CrawlerProcess(get_project_settings())
     process.crawl("taichung_rental")
     process.start()
-
-
-def main():
-    while True:
-        print("Spide start!")
-        run_spider()
-        print("Spider finished! Sleep 5 minutes.")
-        time.sleep(300)
 
 
 if __name__ == "__main__":
