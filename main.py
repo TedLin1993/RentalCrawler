4741c73fdc21b6b7ef77ec99457210ade852912f
Ted <xup6y3ru04@gmail.com>
2025-05-13 21:52:21 +0800

    feat: implement Flask route to trigger Scrapy crawler and update requirements

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+from flask import Flask, Response
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from multiprocessing import Process
+
+app = Flask(__name__)
 
 
-def main():
+def run_crawler():
     process = CrawlerProcess(get_project_settings())
     process.crawl("taichung_rental")
     process.start()
 
 
+@app.route("/run-crawler", methods=["POST"])
+def trigger_crawler():
+    p = Process(target=run_crawler)
+    p.start()
+    return Response("Crawler started", status=202)
+
+
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=8080)
